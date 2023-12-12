6c97e2173d0a2247447865c910d2c93e9228ff51
GATINEL Erwann <ewnran69@yahoo.fr>
2023-12-12 16:50:54 +0100

    change navbar

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ function fermerImage() {
     // Cacher l'image agrandie
     imageAgrandie.style.display = "none";
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const mobileMenuIcon = document.querySelector('.mobile-menu-icon');
+    const navLinks = document.querySelector('.nav-links');
+
+    // Ajouter un écouteur d'événements sur le clic de l'icône du menu
+    mobileMenuIcon.addEventListener('click', function() {
+        // Vérifier si la classe 'active' est déjà présente
+        const isActive = navLinks.classList.contains('active');
+
+        // Si la classe 'active' est présente, la retirer ; sinon, l'ajouter
+        if (isActive) {
+            navLinks.classList.remove('active');
+        } else {
+            navLinks.classList.add('active');
+        }
+    });
+});
